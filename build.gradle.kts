1d392889c24d7070e706739c8378176573c0bc90
quaoz school <74419801+quaoz@users.noreply.github.com>
2022-05-04 14:00:03 +0100

    json experiments for database config

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,5 +11,9 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains:annotations:23.0.0")
-    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
 }
--- src/main/java/com/github/quaoz/tests/DataBaseTester.java
+++ src/main/java/com/github/quaoz/tests/DataBaseTester.java
@@ -1,4 +1,14 @@
 package com.github.quaoz.tests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class DataBaseTester {
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        DataBaseConfig db = objectMapper.readValue(new File("src/main/resources/test-db/db.txt"), DataBaseConfig.class);
+        System.out.println(db.isOrdered());
+    }
 }
